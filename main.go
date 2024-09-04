e181fd85e45b74012de5ffb22a7c1a044387c101
sohan <sohan@appscode.com>
2024-09-04 18:02:41 +0600

    Signed-off-by: sohan <sohan@appscode.com>

--- main.go
+++ main.go
@@ -21,17 +21,18 @@ type DiagnosticOutput struct {
 	Data    []byte
 }
 
-const maxLinesPerOutput = 1
+const maxLinesPerOutput = 2
 
 func main() {
 	// Example data
 	results := []DiagnosticResult{
 		{
-			CheckType: "Type1",
+			CheckType: "InspectLogs",
 			Timestamp: metav1.Now(),
 			Outputs: []DiagnosticOutput{
-				{Message: "Output 1", Data: []byte("Data 1")},
-				{Message: "Output 2", Data: []byte("Data 2")},
+				{Message: "Log 1", Data: []byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n")},
+				{Message: "Log 2", Data: []byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\ngg" +
+					"")},
 			},
 		},
 	}
